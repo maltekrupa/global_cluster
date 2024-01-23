8d8e1ee062e07998b315607b56981657a4d2891b
Malte Krupa <github-nqyroppg@nafn.de>
2024-01-23 11:05:57 +0100

    Run scheduled updates to increment counter
    to speed up the process.

--- lib/global_cluster/visitor_counter.ex
+++ lib/global_cluster/visitor_counter.ex
@@ -37,25 +37,45 @@ defmodule GlobalCluster.VisitorCounter do
   # Server
   @impl GenServer
   def init(_opts) do
+    schedule_update()
+
     {:ok, %{}}
   end
 
   @impl true
   def handle_cast({:increment, node}, state) do
-    current_counter =
-      case :mnesia.transaction(fn -> :mnesia.read({:visitor, node}) end) do
-        {:atomic, []} -> 0
-        {:atomic, x} -> x |> List.first() |> elem(2)
-      end
-
-    :mnesia.transaction(fn ->
-      :mnesia.write({
-        :visitor,
-        node,
-        current_counter + 1
-      })
-    end)
+    state = Map.update(state, node, 1, fn x -> x + 1 end)
 
     {:noreply, state}
+  end
+
+  @impl true
+  def handle_info(:update_database, state) do
+    state
+    |> Enum.each(fn {node, counter} ->
+      current_counter_transaction = fn -> :mnesia.read({:visitor, node}) end
+
+      current_counter =
+        case :mnesia.transaction(current_counter_transaction) do
+          {:atomic, []} -> 0
+          {:atomic, x} -> x |> List.first() |> elem(2)
+        end
+
+      :mnesia.transaction(fn ->
+        :mnesia.write({
+          :visitor,
+          node,
+          current_counter + counter
+        })
+      end)
+    end)
+
+    schedule_update()
+
+    {:noreply, %{}}
+  end
+
+  defp schedule_update do
+    Process.send_after(self(), :update_database, 1_000)
   end
 end
